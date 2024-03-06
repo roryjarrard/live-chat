13b0ef202e5f901794d690cb96c7448c5fca00dd
Rory Jarrard <rjarrard@frontsteps.com>
2024-03-06 08:06:22 -0700

    add logout composable

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,12 +4,30 @@
       <p>display name here</p>
       <p class="email">Currently logged in as ...email</p>
     </div>
-    <button>Logout</button>
+    <button @click.prevent="handleLogout">Logout</button>
   </nav>
 </template>
 
 <script>
-export default {};
+import useLogout from "@/composables/useLogout";
+
+export default {
+  setup() {
+    const { logout, error } = useLogout();
+
+    const handleLogout = async () => {
+      await logout();
+      if (!error.value) {
+        console.log("user logged out");
+      }
+    };
+
+    return {
+      error,
+      handleLogout,
+    };
+  },
+};
 </script>
 
 <style>
